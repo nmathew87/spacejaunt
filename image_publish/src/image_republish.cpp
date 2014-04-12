12e16b9f912d7fe3d770ab355a84a280fabb7c1a
Prasenjit Mukherjee <pmukherj@clearpathrobotics.com>
2014-04-12 14:47:10 -0400

    image republishing working, moved jpgs into ros package to allow ros package finder to find the image

--- image_publish/src/image_republish.cpp
+++ image_publish/src/image_republish.cpp
@@ -13,24 +13,54 @@ using namespace cv;
 using namespace std;
 
 
-void callback(const ros::TimerEvent& event) {
-  Mat image;
-  image = imread("/home/prasenjit/ros_viewpoint/src/spacejaunt/images/earth_from_iss.jpg", CV_LOAD_IMAGE_COLOR);
-  if (!image.data) {
-    ROS_ERROR("Image not found");
+
+class ImageConverter
+{
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Publisher image_pub_;
+  
+public:
+  ImageConverter()
+    : it_(nh_)
+  {
+    // Subscrive to input video feed and publish output video feed
+    image_pub_ = it_.advertise("/target_image", 1);
   }
-  else {
-    imshow("Test", image);
+
+  ~ImageConverter()
+  {
   }
-  waitKey(3); 
-  ROS_INFO("Test");
-}
+
+  void imageCb(const ros::TimerEvent& event)
+  {
+      Mat image;
+      string filename;
+      nh_.param<std::string>("/image_republish/filename", filename, "image.jpg");
+      image = imread(filename, CV_LOAD_IMAGE_COLOR);
+      if (!image.data) {
+        ROS_ERROR("Image:%s not found",filename.c_str());
+      }
+
+      ros::Time time = ros::Time::now();
+      cv_bridge::CvImage cvi;
+      cvi.header.stamp = time;
+      cvi.header.frame_id = "image";
+      cvi.encoding = "bgr8";
+      cvi.image = image;
+
+      sensor_msgs::Image im;
+      cvi.toImageMsg(im);
+      image_pub_.publish(im);
+  }
+};
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "image_republish");
   ros::NodeHandle nh;
-  ros::Timer timer = nh.createTimer(ros::Duration(0.1), callback);
+  ImageConverter ic;
+  ros::Timer timer = nh.createTimer(ros::Duration(0.1), &ImageConverter::imageCb,&ic);
   ros::spin();
   return 0;
 }
